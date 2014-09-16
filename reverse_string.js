ecc41f7c5567622dd5989a81c2c3bcc3b32d4f9c
Patrick <pa@arthurfamily.ws>
2014-09-15 21:18:25 -0400

    reverse string

---
+++ reverse_string.js
@@ -0,0 +1,12 @@
+String.prototype.reverse = function() {
+    var s = "";
+    var i = this.length;
+    while (i>0) {
+        s += this.substring(i-1,i);
+        i--;
+    }
+    return s;
+}
+
+var string = "Patrick"
+string.reverse()
